621b992cc16ee0dde834318e7f5ba94cf1db80a9
sangchoonjung <109258408+sangchoonjung@users.noreply.github.com>
2022-10-07 11:51:55 +0900

    1

--- router/productRouter.js
+++ router/productRouter.js
@@ -17,6 +17,34 @@ router.post("/allProductList", async (req, resp) => {
 
 
 });
+//서치바 이용한 상품 읽어오기
+router.post("/searchProductList", async (req, resp) => {
+    const requestSearchItem = req.body.search;
+    console.log(requestSearchItem);
+    try {
+        const data = await Product.find({ title: { $regex: requestSearchItem }});
+            resp.status(200).json({ result: true, message: data });
+    } catch (e) {
+        console.log(e);
+        resp.status(500).json({ result: false });
+    }
+
+
+})
+//카테고리 선정을 통한 상품 읽어오기
+router.post("/categoryProductList", async (req, resp) => {
+    const requestSearchItem = req.body.category;
+    console.log(requestSearchItem);
+    try {
+        const data = await Product.find({ category: { $regex: requestSearchItem } });
+        resp.status(200).json({ result: true, message: data });
+    } catch (e) {
+        console.log(e);
+        resp.status(500).json({ result: false });
+    }
+
+
+})
 
 
 module.exports = router;
