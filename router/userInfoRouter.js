78758a98ee9ce7f367db77331980452670f7918c
sangchoonjung <109258408+sangchoonjung@users.noreply.github.com>
2022-10-17 13:15:02 +0900

    aa

--- router/userInfoRouter.js
+++ router/userInfoRouter.js
@@ -35,7 +35,7 @@ router.post("/requestReview", async (req, resp) => {
         });
         if (response) {
             const updateBefore = await Account.findOne({ id: data[2][1] }).select("completeReview").lean()
-            // console.log(updateBefore.completeReview, "updateBefore")
+            console.log(updateBefore.completeReview, "updateBefore")
             // console.log(data[0][1].productId, "data[0][1]")
             const updateAfter = await Account.findOneAndUpdate({ id: data[2][1] }, {
                 completeReview: [...updateBefore?.completeReview, data[0][1].productId]
@@ -44,6 +44,7 @@ router.post("/requestReview", async (req, resp) => {
             })
             return resp.status(200).json({ result: true, message: updateAfter });
         }
+        console.log(updateAfter.completeReview, "updateAfter")
         resp.status(401).json({ result: false });
     } catch (e) {
         console.log(e.message);
