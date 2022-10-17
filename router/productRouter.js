c1496448df12065b611ddfa12ad2130c793295c4
sangchoonjung <109258408+sangchoonjung@users.noreply.github.com>
2022-10-17 14:01:41 +0900

    Update productRouter.js

--- router/productRouter.js
+++ router/productRouter.js
@@ -7,17 +7,22 @@ const router = express.Router();
 // 전체 상품 읽어오기
 router.post("/allProductList", async (req, resp) => {
     console.log(req.body);
+
     try {
         const data = await Product.find();
         resp.status(200).json({ result: true, message: data });
     } catch (e) {
         resp.status(401).json({ result: false });
     }
+
+
+
 });
 
 //서치바 이용한 상품 읽어오기
 router.post("/searchProductList", async (req, resp) => {
     const requestSearchItem = req.body.search;
+    console.log(requestSearchItem);
     try {
         const data = await Product.find({ title: { $regex: String(requestSearchItem) } });
         resp.status(200).json({ result: true, message: data });
@@ -25,6 +30,8 @@ router.post("/searchProductList", async (req, resp) => {
         console.log(e);
         resp.status(500).json({ result: false });
     }
+
+
 })
 
 //카테고리 선정을 통한 상품 읽어오기
@@ -37,21 +44,28 @@ router.post("/categoryProductList", async (req, resp) => {
         console.log(e);
         resp.status(500).json({ result: false });
     }
+
+
 })
 
 
 //찜리스트에서 상품 불러오기
 router.post("/zzimProductList", async (req, resp) => {
     // console.log(req.body.zzimList, "req.body")
+
     try {
         const requestSearchItem = req.body.zzimList;
         const itemId = requestSearchItem.map(e => { return e.id })
+        // console.log(itemId)
         const data = await Product.find({ key: { $in: itemId } }).lean();
+        // console.log(data)
+
         const sortedValue = data.map(e => {
             const idx = requestSearchItem.findIndex(elm => elm.id === e.key)
             return { ...e, date: requestSearchItem[idx].date, zzimType: true }
         }
         ).sort((a, b) => a.date - b.date)
+
         console.log(sortedValue, "sortedValue")
         resp.status(200).json({ result: true, message: sortedValue })
     } catch (e) {
@@ -66,16 +80,43 @@ router.post("/requestProductList", async (req, resp) => {
         const requestSearchItem = req.body.list;
         console.log(requestSearchItem)
         const itemId = requestSearchItem.map(e => { return e.productId })
-
+        console.log(itemId,"check 1 !!!!!!!!!!!!!!!!")
         const data = await Product.find({ key: { $in: itemId } }).lean();
         // console.log(data)
         // console.log(req.body.type)
         const sortedValue = data.map(e => {
             const idx = requestSearchItem.findIndex(elm => elm.productId === e.key)
+            console.log(idx,"sangchoon check!!!!!!!!!!!!!")
             return { ...e, date: requestSearchItem[idx].date, unit: requestSearchItem[idx].unit, price: requestSearchItem[idx].price, type: req.body.type }
         }
         ).sort((a, b) => a.date - b.date)
+
         console.log(sortedValue, "sortedValue PENDING!!!!!!!!!!!")
+        console
+        resp.status(200).json({ result: true, message: sortedValue })
+    } catch (e) {
+        console.log(e.message)
+    }
+});
+//pending 수량 수정
+router.post("/requestProductFix", async (req, resp) => {
+    try {
+        const itemfix = req.body.list;
+        console.log(itemfix)
+        const itemId = itemfix.map(e => { return e.productId })
+        console.log(itemId, "아이템 키값")
+        const data = await Product.find({ key: { $in: itemId } }).lean();
+        // console.log(data)
+        // console.log(req.body.type)
+        const sortedValue = data.map(e => {
+            const idx = itemfix.findIndex(elm => elm.productId === e.key)
+            console.log(idx, "sangchoon check!!!!!!!!!!!!!")
+            return { ...e, unit: requestSearchItem[idx].unit, price: requestSearchItem[idx].price, type: req.body.type }
+        }
+        ).sort((a, b) => a.date - b.date)
+
+        console.log(sortedValue, "sortedValue PENDING!!!!!!!!!!!")
+        console
         resp.status(200).json({ result: true, message: sortedValue })
     } catch (e) {
         console.log(e.message)
@@ -84,7 +125,7 @@ router.post("/requestProductList", async (req, resp) => {
 
 
 //complete
-router.post("/requestProductListComplete", async (req, resp) => {
+router.post("/requestProductListComplete",async(req,resp)=>{
     try {
         const requestSearchItem = req.body.list;
         console.log(requestSearchItem)
@@ -106,7 +147,7 @@ router.post("/requestProductListComplete", async (req, resp) => {
     }
 })
 
-//리뷰 가져오기
+//평점 가져오기 (수정중)
 router.post("/requestProductReview", async (req, resp) => {
     try {
         const id = req.body.productId;
@@ -123,27 +164,27 @@ router.post("/requestQnaAdd", async (req, resp) => {
     try {
 
         console.log(req.body)
-        const {qna,productId,userId} = req.body
-        if(qna,productId,userId){
-            return  resp.status(401).json({ result: false });
+        const { qna, productId, userId } = req.body
+        if (qna, productId, userId) {
+            return resp.status(401).json({ result: false });
         }
-        const origin = Product.findOne({key:productId}).select("QnA").lean()
+        const origin = Product.findOne({ key: productId }).select("QnA").lean()
 
-        const updateProduct = await Product.findOneAndUpdate({key:productId},{
-            "QnA":[...origin.QnA,{
-                writer:userId,
-                question:qna,
-                answer:false,
-                questionDate:Date.now()
+        const updateProduct = await Product.findOneAndUpdate({ key: productId }, {
+            "QnA": [...origin.QnA, {
+                writer: userId,
+                question: qna,
+                answer: false,
+                questionDate: Date.now()
             }]
-        },{
-            returnDocument:"after"
+        }, {
+            returnDocument: "after"
         });
-//나중에 findOneAndUpdate 써서 answer업데이트하기
+        //나중에 findOneAndUpdate 써서 answer업데이트하기
 
-if(updateProduct){
-    return resp.status(200).json({ result: true, updateProduct:updateProduct });
-}
+        if (updateProduct) {
+            return resp.status(200).json({ result: true, updateProduct: updateProduct });
+        }
         resp.status(401).json({ result: false });
 
     } catch (e) {
